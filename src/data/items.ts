e87697128a2216e46cb97f4c1238152884413e17
Davi Silva <davisilvameneses@gmail.com>
2023-03-20 08:38:09 -0300

    dev memory andamento b7

---
+++ src/data/items.ts
@@ -0,0 +1,15 @@
+import android from '../svgs/android.svg'
+import caminhao from '../svgs/caminhao.svg'
+import disney from  '../svgs/disney.svg'
+import estrela from '../svgs/estrela.svg'
+import gasolina from '../svgs/gasolina.svg'
+import moto from '../svgs/gasolina.svg'
+
+export const items = [
+  {name: 'android', icon: android },
+  {name: 'android', icon: caminhao },
+  {name: 'android', icon: disney },
+  {name: 'android', icon: estrela },
+  {name: 'android', icon: gasolina },
+  {name: 'android', icon: moto },
+];
